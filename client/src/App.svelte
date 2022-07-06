c19c91843afbc80da22e945d0bded1b13de4bb9d
mihirs16 <mihirs16@gmail.com>
2022-07-07 04:25:00 +0530

    feat: added proper rooms with unique names | feat: added a homepage

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,22 +1,29 @@
 <script lang="ts">
+
+    // ui imports
+    import "carbon-components-svelte/css/white.css";
+
+    // component imports
     import Editor from "./lib/Editor.svelte";
-    
+    import Home from "./lib/Home.svelte";
+   
+    // get roomid from url
     const getRoomID = (): string | false => {
         const paramsRaw = window.location.href.split('?');
         
-        if (!paramsRaw[2]) return false;
-        const paramsParse = new URLSearchParams(paramsRaw[2]);
+        if (!paramsRaw[1]) return false;
+        const paramsParse = new URLSearchParams(paramsRaw[1]);
         const roomID = paramsParse.get('roomid');
         
         if (!roomID) return false;
         else return roomID;
     };
-
     let roomID: string | false = getRoomID();
+
 </script>
 
 {#if roomID == false}
-    <h1></h1>
+    <Home />
 {:else}
     <Editor roomID={roomID}/>
 {/if}
---
+++ client/src/lib/Home.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+    
+    // carbon-ui imports
+    import { 
+        Header,
+        Button,
+        Content,
+        Grid,
+        Row,
+        Column,
+        CodeSnippet
+    } from "carbon-components-svelte";
+
+    // module imports
+    import {
+        adjectives,
+        animals,
+        uniqueNamesGenerator
+    } from "unique-names-generator";
+
+    // create a new room and switch to it
+    const createNewRoom = (): void => {
+        const roomid = uniqueNamesGenerator({
+             dictionaries: [adjectives, animals],
+            separator: "-",
+            length: 2,
+            style: "lowerCase"
+        });
+
+        window.location.href = `https://${window.location.host}?roomid=${roomid}`;
+    };
+
+</script>
+
+<!-- header -->
+<Header style="font-size: x-large;" company="Monte" platformName="Carlo"/>
+
+<!-- main content -->
+<Content style="padding-top: 100px;">
+    <Grid>
+    <Row>
+        <Column>
+            <Row>
+                <Column>
+                    <h1>Premium code editor.</h1>
+                </Column>
+            </Row>
+            <Row>
+                <Column>
+                    <h1>Now free for everyone.</h1>
+                </Column>
+            </Row>
+            <Row style="padding-top: 30px;">
+                <Column>
+                    <p>We re-engineered the service that we didn't build, Monaco, <br/>and made it easy to share and colaborate with.</p>
+                </Column>
+            </Row>
+            <Row style="padding-top: 50px;">
+                <Column>
+                    <Button on:click={createNewRoom}>Create Room</Button>
+                </Column>
+            </Row>
+        </Column>
+        <Column style="padding-top: 50px;">
+            <Row>
+                <Column>
+                    <p>To join a room, use the Room ID as the URL parameter (as shown below).</p>
+                </Column>
+            </Row>
+            <Row style="padding-top: 30px;">
+                <Column>
+                    <CodeSnippet code="https://monte-carlo-editor.netlify.com?roomid=<CODE-HERE>" />
+                </Column>
+            </Row>
+        </Column>
+    </Row>
+    </Grid>
+</Content>
